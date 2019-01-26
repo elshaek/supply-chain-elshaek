a67c397cff85476ef271f39f8ceaa8877296483b
Elsha Kwee <elsha.e.k@gmail.com>
2019-01-26 14:17:44 +0700

    Complete exercise

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,10 +12,12 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping(uint => Item) public items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -24,18 +26,31 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
+  enum State { ForSale, Sold, Shipped, Received }
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
   */
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
     Each event should accept one argument, the sku*/
+  event ForSale(uint sku);
+  event Sold(uint sku);
+  event Shipped(uint sku);
+  event Received(uint sku);
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
-
+  modifier verifySeller(address _seller) { require (msg.sender == _seller); _;}
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 
   modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
@@ -50,15 +65,16 @@ contract SupplyChain {
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
    that the item with the given sku has the state ForSale. */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale(Item memory _item) { require (_item.state == State.ForSale); _;}
+  modifier soldItem(Item memory _item) { require (_item.state == State.Sold); _; }
+  modifier shipped(Item memory _item) { require (_item.state == State.Shipped); _; }
+  modifier received(Item memory _item) { require (_item.state == State.Received); _; }
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+     owner = msg.sender;
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
@@ -76,22 +92,46 @@ contract SupplyChain {
 
   function buyItem(uint sku)
     public
-  {}
+    payable
+    verifyCaller(msg.sender)
+    forSale(items[sku])
+    paidEnough(items[sku].price)
+    checkValue(sku)
+  {
+    items[sku].seller.transfer(items[sku].price);
+    items[sku].buyer = msg.sender;
+    items[sku].state = State.Sold;
+    emit Sold(sku);
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
   function shipItem(uint sku)
     public
-  {}
+    soldItem(items[sku])
+    verifySeller(items[sku].seller)
+  {
+      items[sku].state = State.Shipped;
+      emit Shipped(sku);
+  }
 
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
     public
-  {}
+    shipped(items[sku])
+    verifyCaller(msg.sender)
+  {
+    items[sku].state = State.Received;
+    emit Received(sku);
+  }
 
   /* We have these functions completed so we can run tests, just ignore it :) */
-  function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, uint state, address seller, address buyer) {
+  function fetchItem(uint _sku)
+    public
+    view
+    returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) 
+  {
     name = items[_sku].name;
     sku = items[_sku].sku;
     price = items[_sku].price;
@@ -100,5 +140,4 @@ contract SupplyChain {
     buyer = items[_sku].buyer;
     return (name, sku, price, state, seller, buyer);
   }
-
 }
